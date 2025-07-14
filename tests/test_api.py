ad7f8a5e5da2b71f19bb6428276c32f620eeecd2
ameerricle <nizami.ameer@gmail.com>
2025-07-14 14:52:25 -0400

    Seperated tests for MLP / Ph

--- tests/test_api.py
+++ tests/test_api.py
@@ -34,6 +34,30 @@ def test_gde_optimizer_within():
     ei, next_pick = gde.step_within_data(df_new, df_explore)
 
     print("Second pick", ei, int(next_pick))
+    
+def test_gde_optimizer_within_ML():
+    from carbondriver import GDEOptimizer
+    from carbondriver.loaders import load_data
+
+    import pandas as pd
+    
+    gde = GDEOptimizer("ML", output_dir="./tmp_test_out")
+
+    _, _, _, _, df = load_data(data_path)
+
+    df_train = df.loc[:30]
+    df_explore = df.loc[31:]
+
+    ei, next_pick = gde.step_within_data(df_train, df_explore)
+
+    print("First pick:", ei, int(next_pick))
+    
+    df_new =  df_explore.iloc[int(next_pick)]
+    df_explore =  df_explore.drop(index=df_new.name)
+
+    ei, next_pick = gde.step_within_data(df_new, df_explore)
+
+    print("Second pick", ei, int(next_pick))
 
 def test_gde_optimizer_free():
     from carbondriver import GDEOptimizer
@@ -47,4 +71,15 @@ def test_gde_optimizer_free():
 
     print(ei, next_pick)
 
+def test_gde_optimizer_free():
+    from carbondriver import GDEOptimizer
+    from carbondriver.loaders import load_data
+    
+    gde = GDEOptimizer("ML", output_dir="./tmp_test_out")
+
+    _, _, _, _, df = load_data(data_path)
+
+    ei, next_pick = gde.step(df)
+
+    print(ei, next_pick)
     
